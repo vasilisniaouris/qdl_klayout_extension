cc6a620d2dee2626e46081655185b2fe52ebe11e
vasilisniaouris <vasilisniaouris@gmail.com>
2023-04-21 01:05:00 -0700

    Fixed setup bug.

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# TODO: Add a `pyproject.toml` and remove most writing in here. This is the way!
-# TODO: https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
-
 from setuptools import setup
 
 setup()
